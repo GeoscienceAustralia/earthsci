1a938aeb202b8c30ce3a4237c6243702b3c5ecf5
James Navin <james.navin@ga.gov.au>
2012-08-31 14:41:54 +1000

    Initial commit

---
+++ plugins/au.gov.ga.earthsci.ui/src/au/gov/ga/earthsci/ui/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package au.gov.ga.earthsci.ui;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "au.gov.ga.earthsci.ui.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
