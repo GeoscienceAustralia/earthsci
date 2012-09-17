d0de1598eb8521cc688ad02f06b6f5d994631d20
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-09-17 14:31:52 +1000

    Removed old DataModel test class

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/LifeCycleManager.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/LifeCycleManager.java
@@ -24,9 +24,6 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 
-import au.gov.ga.earthsci.model.DataModelImpl;
-import au.gov.ga.earthsci.model.IDataModel;
-
 /**
  * Registered as the product application 'lifeCycleURI' class, which gets called
  * by the injector at different points in the application lifecycle.
@@ -48,6 +45,5 @@ public class LifeCycleManager
 	void postContextCreate()
 	{
 		context.set(Model.class, (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME));
-		context.set(IDataModel.class, new DataModelImpl());
 	}
 }
