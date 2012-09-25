f3d977e23054b52fc781a4a20d3e34900259f757
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-09-25 14:31:37 +1000

    Added license headers

--- plugins/au.gov.ga.earthsci.core.tests/src/au/gov/ga/earthsci/core/persistence/PersisterTest.java
+++ plugins/au.gov.ga.earthsci.core.tests/src/au/gov/ga/earthsci/core/persistence/PersisterTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package au.gov.ga.earthsci.core.persistence;
 
 import gov.nasa.worldwind.util.WWXML;
@@ -19,6 +34,11 @@ import org.w3c.dom.Element;
 import au.gov.ga.earthsci.core.persistence.ExportableWithAdapter.Adaptable;
 import au.gov.ga.earthsci.core.util.XmlUtil;
 
+/**
+ * JUnit tests for the {@link Persister} class.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
 public class PersisterTest
 {
 	@Test
