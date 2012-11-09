3b0a1f68a9016181aec138ea14ae27725be0dd6a
James Navin <james.navin@ga.gov.au>
2012-11-09 12:01:14 +1100

    Adding message bundle for LazyTreeNodeContentProvider

---
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/tree/Messages.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright 2012 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.application.tree;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Message bundle for tree components
+ */
+public class Messages extends NLS
+{
+	private static final String BUNDLE_NAME = "au.gov.ga.earthsci.application.tree.messages"; //$NON-NLS-1$
+	public static String LazyTreeNodeContentProvider_LoadingMessage;
+	static
+	{
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages()
+	{
+	}
+}
