2b58a5bb8613a6e802a756b3176f12e51a924e5b
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-11-21 11:52:42 +1100

    Called the Injector from the new injectable plugin in the LifeCycleManager, and moved the injection of the WorldWindView to the core Activator

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/LifeCycleManager.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/LifeCycleManager.java
@@ -25,7 +25,7 @@ import org.eclipse.e4.ui.workbench.lifecycle.PreSave;
 import au.gov.ga.earthsci.catalog.part.CatalogTreeLabelProvider;
 import au.gov.ga.earthsci.core.retrieve.RetrievalService;
 import au.gov.ga.earthsci.core.retrieve.RetrievalServiceFactory;
-import au.gov.ga.earthsci.core.worldwind.WorldWindView;
+import au.gov.ga.earthsci.injectable.Injector;
 import au.gov.ga.earthsci.logging.SLF4JE4LoggerBridge;
 
 /**
@@ -43,9 +43,6 @@ public class LifeCycleManager
 	private SLF4JE4LoggerBridge loggerBridge;
 
 	@Inject
-	private ProxyConfigurator proxyConfigurator;
-
-	@Inject
 	private WorldWindConfigurator worldWindConfigurator;
 
 	@Inject
@@ -55,9 +52,6 @@ public class LifeCycleManager
 	private RetrievalServiceFactory retrievalServiceFactory;
 
 	@Inject
-	private WorldWindView worldWindView;
-	
-	@Inject
 	private void initialiseExtensions(IExtensionRegistry registry)
 	{
 		CatalogTreeLabelProvider.loadProviders(registry);
@@ -66,7 +60,7 @@ public class LifeCycleManager
 	@PostContextCreate
 	void postContextCreate()
 	{
-		context.set(WorldWindView.class, worldWindView);
+		Injector.injectIntoContext(context);
 	}
 
 	@PreSave
