b0e9b7c4914917e7020f097733686338f3feafd3
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-11-27 15:51:58 +1100

    - Fixed osgi.install.area bug in webstart application  - Added bundle start level support to the webstart build

---
+++ webstart/src/au/gov/ga/earthsci/ant/StartLevel.java
@@ -0,0 +1,27 @@
+package au.gov.ga.earthsci.ant;
+
+public class StartLevel
+{
+	private String bundle;
+	private int level;
+
+	public String getBundle()
+	{
+		return bundle;
+	}
+
+	public void setBundle(String bundle)
+	{
+		this.bundle = bundle;
+	}
+
+	public int getLevel()
+	{
+		return level;
+	}
+
+	public void setLevel(int level)
+	{
+		this.level = level;
+	}
+}
