ef830692bf19faeb62893b027ae5b6fcaba697fa
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-01-25 14:13:49 +1100

    Added the PaintTaskSceneController, containing the pre/post paint task functionality from the AnimatorSceneController

---
+++ plugins/au.gov.ga.earthsci.core/src/au/gov/ga/earthsci/core/worldwind/PaintTask.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright 2012 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.core.worldwind;
+
+import gov.nasa.worldwind.render.DrawContext;
+
+/**
+ * Task which can be added to the {@link AnimatorSceneController} to run before
+ * or after a repaint.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ * @author James Navin (james.navin@ga.gov.au)
+ */
+public interface PaintTask
+{
+	/**
+	 * Run the task; called by the SceneController.
+	 * 
+	 * @param dc
+	 *            Current {@link DrawContext}
+	 */
+	void run(DrawContext dc);
+}
