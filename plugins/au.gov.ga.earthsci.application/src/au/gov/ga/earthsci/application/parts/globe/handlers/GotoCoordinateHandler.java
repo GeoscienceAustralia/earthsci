16d46db240c0556d9fbf22bf6bba95f3d16de904
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-01-31 11:54:21 +1100

    Added "Go to coordinates" feature

---
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/parts/globe/handlers/GotoCoordinateHandler.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright 2013 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.application.parts.globe.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.widgets.Composite;
+
+import au.gov.ga.earthsci.application.parts.globe.GlobePart;
+
+/**
+ * Handler for the goto coordinates command.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
+public class GotoCoordinateHandler
+{
+	@Execute
+	public void execute(Composite parent, GlobePart globe)
+	{
+		GotoCoordinateDialog dialog = new GotoCoordinateDialog(parent.getShell(), globe.getWorldWindow());
+		dialog.open();
+	}
+}
