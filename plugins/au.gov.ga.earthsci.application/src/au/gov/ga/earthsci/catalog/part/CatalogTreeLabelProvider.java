6109f5f3856f6a9d16928d451abb8ab7d0e8fa17
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-03-18 14:50:21 +1100

    Moved collection classes to new util plugin

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/catalog/part/CatalogTreeLabelProvider.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/catalog/part/CatalogTreeLabelProvider.java
@@ -52,9 +52,9 @@ import au.gov.ga.earthsci.core.model.catalog.ICatalogTreeNode;
 import au.gov.ga.earthsci.core.model.layer.ILayerTreeNode;
 import au.gov.ga.earthsci.core.tree.ITreeNode;
 import au.gov.ga.earthsci.core.util.ILabeled;
-import au.gov.ga.earthsci.core.util.collection.HashSetHashMap;
-import au.gov.ga.earthsci.core.util.collection.SetMap;
 import au.gov.ga.earthsci.core.worldwind.ITreeModel;
+import au.gov.ga.earthsci.util.collection.HashSetHashMap;
+import au.gov.ga.earthsci.util.collection.SetMap;
 import au.gov.ga.earthsci.viewers.IControlProvider;
 
 /**
