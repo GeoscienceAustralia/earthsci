658337fa12f36ac4668576614b0f538190026698
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-03-19 13:41:31 +1100

    - Added an XmlRetrieveIntentHandler, an IntentHandler for XML resources retrieved using the retrieval service  - Added the LayerXmlLoader, an XmlLoader for layer and elevation model definitions

---
+++ plugins/au.gov.ga.earthsci.core/src/au/gov/ga/earthsci/core/intent/XmlRetrieveIntentHandler.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright 2013 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.core.intent;
+
+import gov.nasa.worldwind.util.WWXML;
+
+import java.net.URL;
+
+import javax.inject.Inject;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.w3c.dom.Document;
+
+import au.gov.ga.earthsci.core.retrieve.IRetrievalData;
+import au.gov.ga.earthsci.intent.Intent;
+import au.gov.ga.earthsci.intent.IntentCaller;
+import au.gov.ga.earthsci.intent.xml.XmlLoader;
+import au.gov.ga.earthsci.intent.xml.XmlLoaderManager;
+
+/**
+ * IntentHandler that uses the retrieval service to retrieve the Intent's data,
+ * then loads the data as an XML document, and passes the document to the
+ * {@link XmlLoader} system.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
+public class XmlRetrieveIntentHandler extends AbstractRetrieveIntentHandler
+{
+	@Inject
+	private IEclipseContext context;
+
+	@Override
+	protected void handle(IRetrievalData data, URL url, Intent intent, IntentCaller caller)
+	{
+		try
+		{
+			DocumentBuilder builder = WWXML.createDocumentBuilder(true);
+			Document document = builder.parse(data.getInputStream());
+			Object loaded = XmlLoaderManager.getInstance().load(document, url, intent, context);
+			caller.completed(intent, loaded);
+		}
+		catch (Exception e)
+		{
+			caller.error(intent, e);
+		}
+	}
+}
---
+++ plugins/au.gov.ga.earthsci.core/src/au/gov/ga/earthsci/core/model/layer/LayerXmlLoader.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2013 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.core.model.layer;
+
+import gov.nasa.worldwind.Factory;
+import gov.nasa.worldwind.WorldWind;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.avlist.AVListImpl;
+
+import java.net.URL;
+
+import org.w3c.dom.Document;
+
+import au.gov.ga.earthsci.intent.Intent;
+import au.gov.ga.earthsci.intent.xml.XmlLoader;
+import au.gov.ga.earthsci.intent.xml.XmlLoaderException;
+import au.gov.ga.earthsci.intent.xml.XmlLoaderFilter;
+import au.gov.ga.earthsci.worldwind.common.util.AVKeyMore;
+
+/**
+ * {@link XmlLoader} implementation for layer and elevation model documents.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
+public class LayerXmlLoader implements XmlLoader, XmlLoaderFilter
+{
+	@Override
+	public boolean canLoad(Document document, Intent intent)
+	{
+		return "Layer".equalsIgnoreCase(document.getDocumentElement().getNodeName()) //$NON-NLS-1$
+				|| "ElevationModel".equalsIgnoreCase(document.getDocumentElement().getNodeName()); //$NON-NLS-1$
+	}
+
+	@Override
+	public Object load(Document document, URL context, Intent intent) throws XmlLoaderException
+	{
+		AVList params = new AVListImpl();
+		params.setValue(AVKeyMore.CONTEXT_URL, context);
+		try
+		{
+			Factory factory = (Factory) WorldWind.createConfigurationComponent(AVKey.LAYER_FACTORY);
+			return factory.createFromConfigSource(document.getDocumentElement(), params);
+		}
+		catch (Exception e)
+		{
+			throw new XmlLoaderException(e);
+		}
+	}
+}
