0e08f3c130f1a3bdd185f38307327bdbc0cb35ed
James Navin <james.navin@ga.gov.au>
2013-04-03 13:12:20 +1100

    Adding an intent handler for GDAL raster surfaces

--- plugins/au.gov.ga.earthsci.core/src/au/gov/ga/earthsci/model/intent/GDALRasterModelIntentHandler.java
+++ plugins/au.gov.ga.earthsci.core/src/au/gov/ga/earthsci/model/intent/GDALRasterModelIntentHandler.java
@@ -15,29 +15,28 @@
  ******************************************************************************/
 package au.gov.ga.earthsci.model.intent;
 
+import gov.nasa.worldwind.util.gdal.GDALUtils;
+
+import java.io.File;
 import java.net.URL;
 
-import javax.inject.Inject;
+import org.gdal.gdal.Dataset;
 
-import au.gov.ga.earthsci.core.retrieve.IRetrieval;
-import au.gov.ga.earthsci.core.retrieve.IRetrievalService;
-import au.gov.ga.earthsci.core.retrieve.RetrievalAdapter;
 import au.gov.ga.earthsci.intent.IIntentCallback;
 import au.gov.ga.earthsci.intent.IIntentHandler;
 import au.gov.ga.earthsci.intent.Intent;
+import au.gov.ga.earthsci.model.IModel;
+import au.gov.ga.earthsci.worldwind.common.util.URLUtil;
 
 /**
- * An intent handler that responds to intents that match GDAL-supported raster formats
- * and generates an IModel
+ * An intent handler that responds to intents that match GDAL-supported raster
+ * formats and generates an IModel
  * 
  * @author James Navin (james.navin@ga.gov.au)
  */
 public class GDALRasterModelIntentHandler implements IIntentHandler
 {
 
-	@Inject
-	private IRetrievalService retrievalService;
-	
 	@Override
 	public void handle(final Intent intent, final IIntentCallback callback)
 	{
@@ -48,23 +47,45 @@ public class GDALRasterModelIntentHandler implements IIntentHandler
 			{
 				throw new IllegalArgumentException("Intent URL is null"); //$NON-NLS-1$
 			}
-			
-			IRetrieval retrieval = retrievalService.retrieve(this, url);
-			retrieval.addListener(new RetrievalAdapter () {
-				@Override
-				public void complete(IRetrieval retrieval)
-				{
-					System.out.println("Booya " + url);
-					callback.completed(retrieval.getData(), intent);
-				}
-			});
-			retrieval.start();
-			
+
+			// TODO Use retrieval service to retrieve URL and attach model creation to the completed
+			// lifecycle phase. This requires Issue #15 to be addressed so that a File object can
+			// be obtained from the retrieval result.
+
+			if (!URLUtil.isFileUrl(url))
+			{
+				throw new IllegalArgumentException("Currently only file:// URLs are supported for this feature"); //$NON-NLS-1$
+			}
+
+			File source = URLUtil.urlToFile(url);
+			IModel result = createModel(source);
+			callback.completed(result, intent);
 		}
 		catch (Exception e)
 		{
 			callback.error(e, intent);
 		}
+	}
+
+	/**
+	 * Create an {@link IModel} instance from the GDAL raster referenced by the
+	 * provided file
+	 * 
+	 * @param source
+	 *            The source raster to load
+	 * 
+	 * @return A created {@link IModel} instance, or <code>null</code> if one
+	 *         could not be created
+	 * 
+	 * @throws Exception
+	 *             If something goes wrong during creation
+	 */
+	private IModel createModel(File source) throws Exception
+	{
+		Dataset dataset = GDALUtils.open(source);
+
+		System.out.println("You gave me a " + dataset.GetDriver().getShortName());
+		return null;
 	}
 
 }
