9698d2d70c07f4e771ad0ac506ad09061d9c4032
James Navin <james.navin@ga.gov.au>
2013-04-05 10:37:45 +1100

    Adding a check on BufferType to test if an object can be used in buffers of that type

--- plugins/au.gov.ga.earthsci.model/src/au/gov/ga/earthsci/model/data/BufferType.java
+++ plugins/au.gov.ga.earthsci.model/src/au/gov/ga/earthsci/model/data/BufferType.java
@@ -7,22 +7,36 @@ package au.gov.ga.earthsci.model.data;
  */
 public enum BufferType
 {
-	BYTE(1),
-	SHORT(2),
-	INT(4),
-	LONG(8),
-	FLOAT(4),
-	DOUBLE(8);
-	
+	BYTE(1, Byte.class),
+	SHORT(2, Short.class),
+	INT(4, Integer.class),
+	LONG(8, Long.class),
+	FLOAT(4, Float.class),
+	DOUBLE(8, Double.class);
+
 	private final int numBytes;
-	
-	private BufferType(int numBytes)
+	private final Class<?> typeClass;
+
+	private BufferType(int numBytes, Class<?> typeClass)
 	{
 		this.numBytes = numBytes;
+		this.typeClass = typeClass;
 	}
-	
+
+	/**
+	 * @return The number of bytes used to represent this type of value in a
+	 *         byte buffer/array etc.
+	 */
 	public int getNumberOfBytes()
 	{
 		return numBytes;
+	}
+
+	/**
+	 * @return Whether the given object can be assigned to values of this type
+	 */
+	public boolean isAssignableFrom(Object testObject)
+	{
+		return typeClass.isAssignableFrom(testObject.getClass());
 	}
 }
