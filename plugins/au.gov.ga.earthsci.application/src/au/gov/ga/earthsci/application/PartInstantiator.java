2f084930e718a0af5a2e7b16c058c632294f078c
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-04-09 08:10:34 +1000

    Added class comments

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/PartInstantiator.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/PartInstantiator.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package au.gov.ga.earthsci.application;
 
 import java.util.List;
@@ -19,6 +34,13 @@ import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Injected class which listens for the
+ * {@link UIEvents.UILifeCycle#APP_STARTUP_COMPLETE} event, and then
+ * instantiates parts for any placeholders with the same element id as the part.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
 public class PartInstantiator
 {
 	private Logger logger = LoggerFactory.getLogger(PartInstantiator.class);
