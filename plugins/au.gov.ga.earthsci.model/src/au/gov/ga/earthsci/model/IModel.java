683fd9dc17d72f0bb43eea384acb13eefd9501f5
James Navin <james.navin@ga.gov.au>
2013-04-08 10:13:11 +1000

    Removing IDirtyable from IModel

--- plugins/au.gov.ga.earthsci.model/src/au/gov/ga/earthsci/model/IModel.java
+++ plugins/au.gov.ga.earthsci.model/src/au/gov/ga/earthsci/model/IModel.java
@@ -3,7 +3,6 @@ package au.gov.ga.earthsci.model;
 import java.util.Collection;
 
 import au.gov.ga.earthsci.common.util.IDescribed;
-import au.gov.ga.earthsci.common.util.IDirtyable;
 import au.gov.ga.earthsci.common.util.IIdentifiable;
 import au.gov.ga.earthsci.common.util.INamed;
 import au.gov.ga.earthsci.model.geometry.IModelGeometry;
@@ -14,7 +13,7 @@ import au.gov.ga.earthsci.model.geometry.IModelGeometry;
  * 
  * @author James Navin (james.navin@ga.gov.au)
  */
-public interface IModel extends IIdentifiable, INamed, IDescribed, IDirtyable
+public interface IModel extends IIdentifiable, INamed, IDescribed
 {
 	/**
 	 * Return the geometries associated with this model
@@ -22,15 +21,15 @@ public interface IModel extends IIdentifiable, INamed, IDescribed, IDirtyable
 	 * @return The collection of geometries that make up this model
 	 */
 	Collection<IModelGeometry> getGeometries();
-	
+
 	/**
 	 * Return the geometry with the given ID, if one exists on this model.
 	 * 
-	 * @param id The ID of the geometry to return.
+	 * @param id
+	 *            The ID of the geometry to return.
 	 * 
-	 * @return The geometry with the given ID, or <code>null</code> if no geometry
-	 * with that ID exists on this model
+	 * @return The geometry with the given ID, or <code>null</code> if no
+	 *         geometry with that ID exists on this model
 	 */
 	IModelGeometry getGeometry(String id);
 }
-
