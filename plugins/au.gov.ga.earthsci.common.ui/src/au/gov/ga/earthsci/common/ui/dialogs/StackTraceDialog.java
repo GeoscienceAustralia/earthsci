f303ccc9f74b80d65d4489cccbf80c28aa406d08
James Navin <james.navin@ga.gov.au>
2013-07-01 14:08:14 +1000

    Improving stacktrace output and copy handling for StackTraceDialog (closes #56)

--- plugins/au.gov.ga.earthsci.common.ui/src/au/gov/ga/earthsci/common/ui/dialogs/StackTraceDialog.java
+++ plugins/au.gov.ga.earthsci.common.ui/src/au/gov/ga/earthsci/common/ui/dialogs/StackTraceDialog.java
@@ -17,8 +17,15 @@ package au.gov.ga.earthsci.common.ui.dialogs;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
 /**
@@ -30,6 +37,9 @@ import org.eclipse.swt.widgets.Shell;
 public class StackTraceDialog extends ErrorDialog
 {
 	private IStatus status;
+
+	private Clipboard clipboard;
+	private StringBuffer copyBuffer;
 
 	public StackTraceDialog(Shell parentShell, String dialogTitle, String message, IStatus status, int displayMask)
 	{
@@ -49,18 +59,89 @@ public class StackTraceDialog extends ErrorDialog
 		return dialog.open();
 	}
 
+	@SuppressWarnings("nls")
 	@Override
 	protected List createDropDownList(Composite parent)
 	{
 		List list = super.createDropDownList(parent);
+		copyBuffer = new StringBuffer();
+		copyBuffer.append(status.getMessage());
 		if (status != null && status.getException() != null)
 		{
-			StackTraceElement[] elements = status.getException().getStackTrace();
-			for (StackTraceElement element : elements)
-			{
-				list.add("    " + element.toString()); //$NON-NLS-1$
-			}
+			copyBuffer.append("\n\n");
+			list.add("");
+			addThrowable(list, status.getException(), true);
 		}
+
+		// Replace the default copy behaviour with one that copies the stack trace to the clipboard
+		MenuItem copyItem = list.getMenu().getItems()[0];
+		copyItem.removeListener(SWT.Selection, copyItem.getListeners(SWT.Selection)[0]);
+		copyItem.addSelectionListener(new SelectionListener()
+		{
+
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				copyToClipboard();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e)
+			{
+				copyToClipboard();
+			}
+
+		});
+
 		return list;
+	}
+
+	@SuppressWarnings("nls")
+	private void addThrowable(List list, Throwable t, boolean root)
+	{
+		String str = "";
+		if (!root)
+		{
+			str += "Caused by: ";
+		}
+		str += t.getClass().getCanonicalName() + ": " + t.getLocalizedMessage();
+
+		list.add(str);
+		copyBuffer.append(str).append('\n');
+
+		StackTraceElement[] elements = status.getException().getStackTrace();
+		for (StackTraceElement element : elements)
+		{
+			str = "     at " + element.toString(); //$NON-NLS-1$
+			list.add(str);
+			copyBuffer.append(str).append('\n');
+		}
+
+		if (t.getCause() != null)
+		{
+			addThrowable(list, t.getCause(), false);
+		}
+
+	}
+
+	private void copyToClipboard()
+	{
+		if (clipboard != null)
+		{
+			clipboard.dispose();
+		}
+		clipboard = new Clipboard(getShell().getDisplay());
+		clipboard.setContents(new Object[] { copyBuffer.toString() },
+				new Transfer[] { TextTransfer.getInstance() });
+	}
+
+	@Override
+	public boolean close()
+	{
+		if (clipboard != null)
+		{
+			clipboard.dispose();
+		}
+		return super.close();
 	}
 }
