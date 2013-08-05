d0d74dd64952336f5325a486d984cccc59467e8f
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-08-06 09:58:58 +1000

    Added some power of 2 utility functions

--- plugins/au.gov.ga.earthsci.common/src/au/gov/ga/earthsci/common/util/MathUtil.java
+++ plugins/au.gov.ga.earthsci.common/src/au/gov/ga/earthsci/common/util/MathUtil.java
@@ -64,4 +64,50 @@ public class MathUtil
 		return Math.max(min, Math.min(max, value));
 	}
 
+	/**
+	 * Is x a power of 2?
+	 * 
+	 * @param x
+	 * @return True if x is a power of 2
+	 */
+	public static boolean isPowerOfTwo(long x)
+	{
+		return x != 0 && (x & (x - 1)) == 0;
+	}
+
+	/**
+	 * Calculate the previous power of 2. If x is a power of 2, x is returned,
+	 * otherwise the greatest power of two that is less than x is returned.
+	 * 
+	 * @param x
+	 * @return Greatest power of two less than or equal to x
+	 */
+	public static int previousPowerOfTwo(int x)
+	{
+		x = x | (x >> 1);
+		x = x | (x >> 2);
+		x = x | (x >> 4);
+		x = x | (x >> 8);
+		x = x | (x >> 16);
+		return x - (x >> 1);
+	}
+
+	/**
+	 * Calculate the next power of 2. If x is a power of 2, x is returned,
+	 * otherwise the smallest power of two that is greater than x is returned.
+	 * 
+	 * @param x
+	 * @return Smallest power of two greater than or equal to x
+	 */
+	public static int nextPowerOfTwo(int x)
+	{
+		x--;
+		x |= x >> 1;
+		x |= x >> 2;
+		x |= x >> 4;
+		x |= x >> 8;
+		x |= x >> 16;
+		x++;
+		return x;
+	}
 }
