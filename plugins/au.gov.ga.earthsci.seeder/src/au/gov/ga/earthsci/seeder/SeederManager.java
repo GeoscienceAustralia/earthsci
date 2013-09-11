39329e3e3f27052ed71b3d027064d1b171611ae4
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-09-11 11:56:06 +1000

    Added support for seed file revisions

--- plugins/au.gov.ga.earthsci.seeder/src/au/gov/ga/earthsci/seeder/SeederManager.java
+++ plugins/au.gov.ga.earthsci.seeder/src/au/gov/ga/earthsci/seeder/SeederManager.java
@@ -15,11 +15,20 @@
  ******************************************************************************/
 package au.gov.ga.earthsci.seeder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.xml.parsers.DocumentBuilder;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.RegistryFactory;
@@ -29,9 +38,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
+import au.gov.ga.earthsci.common.persistence.Exportable;
+import au.gov.ga.earthsci.common.persistence.Persistent;
+import au.gov.ga.earthsci.common.persistence.Persister;
+import au.gov.ga.earthsci.common.util.ConfigurationUtil;
+import au.gov.ga.earthsci.common.util.XmlUtil;
 import au.gov.ga.earthsci.injectable.ExtensionPointHelper;
 
 /**
@@ -40,6 +52,7 @@ import au.gov.ga.earthsci.injectable.ExtensionPointHelper;
  * 
  * @author Michael de Hoog (michael.dehoog@ga.gov.au)
  */
+@Singleton
 public class SeederManager
 {
 	public static final String SEEDERS_EXTENSION_POINT_ID = "au.gov.ga.earthsci.seeders"; //$NON-NLS-1$
@@ -51,8 +64,20 @@ public class SeederManager
 	private static final Map<String, Class<ISeeder>> seeders = new HashMap<String, Class<ISeeder>>();
 	private static final Map<Class<ISeeder>, ISeeder> seederCache = new HashMap<Class<ISeeder>, ISeeder>();
 
+	private static final String seederFilename = "seeder.xml"; //$NON-NLS-1$
+	private static final File seederFile = ConfigurationUtil.getWorkspaceFile(seederFilename);
+	private static final Persister persister;
+
+	private static SeederHistory history = new SeederHistory();
+
 	static
 	{
+		persister = new Persister();
+		persister.setIgnoreMissing(true);
+		persister.setIgnoreNulls(true);
+		persister.registerNamedExportable(SeederHistory.class, "History"); //$NON-NLS-1$
+		persister.registerNamedExportable(SeederRevision.class, "Seeder"); //$NON-NLS-1$
+
 		IConfigurationElement[] config =
 				RegistryFactory.getRegistry().getConfigurationElementsFor(SEEDERS_EXTENSION_POINT_ID);
 		for (IConfigurationElement element : config)
@@ -86,29 +111,146 @@ public class SeederManager
 		seeders.remove(elementName);
 	}
 
-	public static void seed(Document document, URL context)
+	public static void seed(Document document, URL url)
 	{
+		int lastRevision = history.getRevision(url);
+		int maxRevision = -1;
+
 		Element documentElement = document.getDocumentElement();
-		NodeList children = documentElement.getChildNodes();
-		for (int i = 0; i < children.getLength(); i++)
+		Element[] children = XmlUtil.getElements(documentElement);
+		for (Element child : children)
 		{
-			Node node = children.item(i);
-			if (!(node instanceof Element))
+			int revision = -1;
+			try
 			{
-				continue;
+				String revisionString = child.getAttribute("revision"); //$NON-NLS-1$
+				revision = Integer.parseInt(revisionString);
 			}
-			Element child = (Element) node;
-			Class<ISeeder> seederClass = seeders.get(child.getNodeName());
-			if (seederClass != null)
+			catch (Exception e)
 			{
-				ISeeder seeder = seederCache.get(seederClass);
-				if (seeder == null)
+				//ignore
+			}
+			maxRevision = Math.max(maxRevision, revision);
+
+			//if newer than last, then apply this revision
+			if (revision > lastRevision)
+			{
+				Class<ISeeder> seederClass = seeders.get(child.getNodeName());
+				if (seederClass != null)
 				{
-					seeder = ContextInjectionFactory.make(seederClass, SeederManager.context);
-					seederCache.put(seederClass, seeder);
+					ISeeder seeder = seederCache.get(seederClass);
+					if (seeder == null)
+					{
+						seeder = ContextInjectionFactory.make(seederClass, context);
+						seederCache.put(seederClass, seeder);
+					}
+					seeder.seed(child, url);
 				}
-				seeder.seed(child, context);
 			}
 		}
+
+		history.setRevision(url, maxRevision);
+	}
+
+	@PostConstruct
+	public void loadSeederFile()
+	{
+		try
+		{
+			if (seederFile.exists())
+			{
+				Document document = XmlUtil.createDocumentBuilder().parse(seederFile);
+				Element parent = document.getDocumentElement();
+				Element element = XmlUtil.getFirstChildElement(parent);
+				history = (SeederHistory) persister.load(element, seederFile.toURI());
+			}
+		}
+		catch (Exception e)
+		{
+			logger.warn("Error loading seeder configuration file", e); //$NON-NLS-1$
+		}
+	}
+
+	@PreDestroy
+	protected void saveSeederFile()
+	{
+		OutputStream os = null;
+		try
+		{
+			DocumentBuilder documentBuilder = XmlUtil.createDocumentBuilder();
+			Document document = documentBuilder.newDocument();
+			Element element = document.createElement("Seeder"); //$NON-NLS-1$
+			document.appendChild(element);
+			persister.save(history, element, seederFile.toURI());
+			os = new FileOutputStream(seederFile);
+			XmlUtil.saveDocumentToFormattedStream(document, os);
+		}
+		catch (Exception e)
+		{
+			logger.error("Error saving seeder configuration file", e); //$NON-NLS-1$
+		}
+		finally
+		{
+			if (os != null)
+			{
+				try
+				{
+					os.close();
+				}
+				catch (IOException e)
+				{
+				}
+			}
+		}
+	}
+
+	@Exportable
+	private static class SeederHistory
+	{
+		private final Map<URL, SeederRevision> map = new HashMap<URL, SeederRevision>();
+
+		@Persistent
+		public SeederRevision[] getRevisions()
+		{
+			Collection<SeederRevision> values = map.values();
+			return values.toArray(new SeederRevision[values.size()]);
+		}
+
+		//used by persistence mechanism
+		@SuppressWarnings("unused")
+		public void setRevisions(SeederRevision[] revisions)
+		{
+			map.clear();
+			if (revisions != null)
+			{
+				for (SeederRevision revision : revisions)
+				{
+					map.put(revision.url, revision);
+				}
+			}
+		}
+
+		public int getRevision(URL url)
+		{
+			SeederRevision revision = map.get(url);
+			return revision != null ? revision.revision : -1;
+		}
+
+		public void setRevision(URL url, int revision)
+		{
+			SeederRevision value = new SeederRevision();
+			value.url = url;
+			value.revision = revision;
+			map.put(url, value);
+		}
+	}
+
+	@Exportable
+	private static class SeederRevision
+	{
+		@Persistent(attribute = true)
+		public URL url;
+		@Persistent(attribute = true)
+		public int revision;
 	}
 }
