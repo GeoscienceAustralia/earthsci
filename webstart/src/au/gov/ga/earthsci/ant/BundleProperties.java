5e7f8ead9f7f5108b822f0266c92fe710d96c850
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-09-12 16:34:45 +1000

    Better logic for parsing os/archs

--- webstart/src/au/gov/ga/earthsci/ant/BundleProperties.java
+++ webstart/src/au/gov/ga/earthsci/ant/BundleProperties.java
@@ -17,6 +17,7 @@ package au.gov.ga.earthsci.ant;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
@@ -84,60 +85,74 @@ public class BundleProperties
 
 	private String[] calculateOsArchs()
 	{
+		MultiMap<String, String> properties = new MultiMap<String, String>();
+
 		if (platformFilter != null)
 		{
 			Pattern propertyPattern = Pattern.compile("\\(([\\w.]+)=([\\w.]+)\\)");
 			Matcher matcher = propertyPattern.matcher(platformFilter);
 			int start = 0;
-			MultiMap<String, String> properties = new MultiMap<String, String>();
 			while (matcher.find(start))
 			{
 				properties.putSingle(matcher.group(1), matcher.group(2));
 				start = matcher.end();
 			}
+		}
 
-			List<String> oss = properties.get("osgi.os");
-			List<String> archs = properties.get("osgi.arch");
-			//on some 64-bit platforms, webstart uses x86_64, others use amd64, so add both
-			if (archs != null)
+		List<String> oss = properties.get("osgi.os");
+		List<String> archs = properties.get("osgi.arch");
+		oss = oss != null ? oss : new ArrayList<String>();
+		archs = archs != null ? archs : new ArrayList<String>();
+
+		//on some 64-bit platforms, webstart uses x86_64, others use amd64, so add both
+		boolean x86_64 = archs.contains("x86_64");
+		boolean amd64 = archs.contains("amd64");
+		if (x86_64 || amd64)
+		{
+			if (!x86_64)
 			{
-				boolean x86_64 = archs.contains("x86_64");
-				boolean amd64 = archs.contains("amd64");
-				if (x86_64 || amd64)
-				{
-					if (!x86_64)
-					{
-						archs.add("x86_64");
-					}
-					if (!amd64)
-					{
-						archs.add("amd64");
-					}
-				}
+				archs.add("x86_64");
 			}
-			if (oss != null && !oss.isEmpty())
+			if (!amd64)
 			{
-				String[] combinations = new String[oss.size() * Math.max(1, archs == null ? 0 : archs.size())];
-				int i = 0;
-				for (String os : oss)
-				{
-					os = Util.getJnlpOs(os);
-					if (archs != null && !archs.isEmpty())
-					{
-						for (String arch : archs)
-						{
-							arch = Util.getJnlpArch(arch);
-							combinations[i++] = " os=\"" + os + "\" arch=\"" + arch + "\"";
-						}
-					}
-					else
-					{
-						combinations[i++] = " os=\"" + os + "\"";
-					}
-				}
-				return combinations;
+				archs.add("amd64");
 			}
 		}
+
+		if (!oss.isEmpty())
+		{
+			String[] combinations = new String[oss.size() * Math.max(1, archs.size())];
+			int i = 0;
+			for (String os : oss)
+			{
+				os = Util.getJnlpOs(os);
+				if (!archs.isEmpty())
+				{
+					for (String arch : archs)
+					{
+						arch = Util.getJnlpArch(arch);
+						combinations[i++] = " os=\"" + os + "\" arch=\"" + arch + "\"";
+					}
+				}
+				else
+				{
+					combinations[i++] = " os=\"" + os + "\"";
+				}
+			}
+			return combinations;
+		}
+		if (!archs.isEmpty())
+		{
+			String[] combinations = new String[archs.size()];
+			int i = 0;
+			for (String arch : archs)
+			{
+				arch = Util.getJnlpArch(arch);
+				combinations[i++] = " arch=\"" + arch + "\"";
+			}
+			return combinations;
+		}
+
 		return new String[] { "" };
 	}
 }
