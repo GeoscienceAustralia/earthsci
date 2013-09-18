b97a633ef50b5aa202e6033cd4f53517c81cef1b
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-09-18 10:17:19 +1000

    Moved the relative file calcuation to the Util class

--- webstart/src/au/gov/ga/earthsci/ant/GenerateSignScript.java
+++ webstart/src/au/gov/ga/earthsci/ant/GenerateSignScript.java
@@ -115,7 +115,7 @@ public class GenerateSignScript extends Task
 				File relative;
 				try
 				{
-					relative = getRelativeFile(file, scriptDirectory);
+					relative = Util.getRelativeFile(file, scriptDirectory);
 				}
 				catch (IOException e)
 				{
@@ -135,7 +135,7 @@ public class GenerateSignScript extends Task
 		String keystore = this.keystore;
 		try
 		{
-			File relativeKeystore = getRelativeFile(new File(keystore), scriptDirectory);
+			File relativeKeystore = Util.getRelativeFile(new File(keystore), scriptDirectory);
 			keystore = relativeKeystore.getPath();
 		}
 		catch (IOException e)
@@ -174,50 +174,5 @@ public class GenerateSignScript extends Task
 		{
 			throw new BuildException("Error writing sign script file", e);
 		}
-	}
-
-	/**
-	 * From <a
-	 * href="http://stackoverflow.com/a/1269907">http://stackoverflow.com
-	 * /a/1269907</a>.
-	 * 
-	 * @param target
-	 *            File to relativize
-	 * @param baseDirectory
-	 *            Base directory to relativize to (treated as a directory:
-	 *            /dir/file.txt treated as /dir/file.txt/)
-	 * @return Relativized file
-	 * @throws IOException
-	 *             If an IOException occurs when calling
-	 *             {@link File#getCanonicalPath()}
-	 */
-	private static File getRelativeFile(File target, File baseDirectory) throws IOException
-	{
-		String[] baseComponents = baseDirectory.getCanonicalPath().split(Pattern.quote(File.separator));
-		String[] targetComponents = target.getCanonicalPath().split(Pattern.quote(File.separator));
-
-		// skip common components
-		int index = 0;
-		for (; index < targetComponents.length && index < baseComponents.length; ++index)
-		{
-			if (!targetComponents[index].equals(baseComponents[index]))
-				break;
-		}
-
-		StringBuilder result = new StringBuilder();
-		if (index != baseComponents.length)
-		{
-			// backtrack to base directory
-			for (int i = index; i < baseComponents.length; ++i)
-				result.append(".." + File.separator);
-		}
-		for (; index < targetComponents.length; ++index)
-			result.append(targetComponents[index] + File.separator);
-		if (!target.getPath().endsWith("/") && !target.getPath().endsWith("\\"))
-		{
-			// remove final path separator
-			result.delete(result.length() - File.separator.length(), result.length());
-		}
-		return new File(result.toString());
 	}
 }
--- webstart/src/au/gov/ga/earthsci/ant/Util.java
+++ webstart/src/au/gov/ga/earthsci/ant/Util.java
@@ -15,8 +15,11 @@
  ******************************************************************************/
 package au.gov.ga.earthsci.ant;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Helper utility class.
@@ -51,5 +54,50 @@ public class Util
 			return archMap.get(arch);
 		}
 		return arch;
+	}
+	
+	/**
+	 * From <a
+	 * href="http://stackoverflow.com/a/1269907">http://stackoverflow.com
+	 * /a/1269907</a>.
+	 * 
+	 * @param target
+	 *            File to relativize
+	 * @param baseDirectory
+	 *            Base directory to relativize to (treated as a directory:
+	 *            /dir/file.txt treated as /dir/file.txt/)
+	 * @return Relativized file
+	 * @throws IOException
+	 *             If an IOException occurs when calling
+	 *             {@link File#getCanonicalPath()}
+	 */
+	public static File getRelativeFile(File target, File baseDirectory) throws IOException
+	{
+		String[] baseComponents = baseDirectory.getCanonicalPath().split(Pattern.quote(File.separator));
+		String[] targetComponents = target.getCanonicalPath().split(Pattern.quote(File.separator));
+
+		// skip common components
+		int index = 0;
+		for (; index < targetComponents.length && index < baseComponents.length; ++index)
+		{
+			if (!targetComponents[index].equals(baseComponents[index]))
+				break;
+		}
+
+		StringBuilder result = new StringBuilder();
+		if (index != baseComponents.length)
+		{
+			// backtrack to base directory
+			for (int i = index; i < baseComponents.length; ++i)
+				result.append(".." + File.separator);
+		}
+		for (; index < targetComponents.length; ++index)
+			result.append(targetComponents[index] + File.separator);
+		if (!target.getPath().endsWith("/") && !target.getPath().endsWith("\\"))
+		{
+			// remove final path separator
+			result.delete(result.length() - File.separator.length(), result.length());
+		}
+		return new File(result.toString());
 	}
 }
