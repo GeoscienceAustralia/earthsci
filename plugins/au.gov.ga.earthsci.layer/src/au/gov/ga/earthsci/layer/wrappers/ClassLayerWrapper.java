dd41d8abedb01111319d859d9bbb6c3bd74720d0
Michael de Hoog <michael.dehoog@ga.gov.au>
2013-10-30 13:08:31 +1100

    Added ILayer wrapper for the SkyGradientLayer

---
+++ plugins/au.gov.ga.earthsci.layer/src/au/gov/ga/earthsci/layer/wrappers/ClassLayerWrapper.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright 2013 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.layer.wrappers;
+
+import gov.nasa.worldwind.layers.Layer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+
+import au.gov.ga.earthsci.layer.ILayerWrapper;
+import au.gov.ga.earthsci.layer.LayerDelegate;
+import au.gov.ga.earthsci.layer.ExtensionManager;
+
+/**
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ * 
+ */
+public abstract class ClassLayerWrapper<L extends Layer> extends LayerDelegate implements ILayerWrapper
+{
+	private static final Logger logger = LoggerFactory.getLogger(ExtensionManager.class);
+	private final static String LAYER_CLASS_ATTRIBUTE = "layerClass"; //$NON-NLS-1$
+
+	protected abstract Class<L> getLayerClass();
+
+	protected abstract void load(L layer, Element element);
+
+	protected abstract void save(L layer, Element element);
+
+	@Override
+	public final void load(Element parent)
+	{
+		try
+		{
+			@SuppressWarnings("unchecked")
+			Class<? extends L> layerClass =
+					(Class<? extends L>) Class.forName(parent.getAttribute(LAYER_CLASS_ATTRIBUTE));
+			if (!getLayerClass().isAssignableFrom(layerClass))
+			{
+				layerClass = getLayerClass();
+			}
+			L layer = layerClass.newInstance();
+			setLayer(layer);
+			load(layer, parent);
+		}
+		catch (Exception e)
+		{
+			logger.error("Error loading layer", e); //$NON-NLS-1$
+		}
+	}
+
+	@Override
+	public final void save(Element parent)
+	{
+		L layer = getLayer();
+		save(layer, parent);
+		parent.setAttribute(LAYER_CLASS_ATTRIBUTE, layer.getClass().getName());
+	}
+
+	@Override
+	public boolean isLoading()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean supports(Layer layer)
+	{
+		return getLayerClass().isAssignableFrom(layer.getClass());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public L getLayer()
+	{
+		return (L) super.getLayer();
+	}
+
+	@Override
+	public void setLayer(Layer layer)
+	{
+		if (!supports(layer))
+		{
+			throw new IllegalArgumentException();
+		}
+		super.setLayer(layer);
+	}
+}
---
+++ plugins/au.gov.ga.earthsci.layer/src/au/gov/ga/earthsci/layer/wrappers/SkyGradientLayerWrapper.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright 2013 Geoscience Australia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package au.gov.ga.earthsci.layer.wrappers;
+
+import gov.nasa.worldwind.layers.SkyGradientLayer;
+import gov.nasa.worldwind.util.WWXML;
+
+import java.awt.Color;
+
+import javax.xml.xpath.XPath;
+
+import org.w3c.dom.Element;
+
+import au.gov.ga.earthsci.layer.ILayerWrapper;
+import au.gov.ga.earthsci.worldwind.common.util.XMLUtil;
+
+/**
+ * {@link ILayerWrapper} for {@link SkyGradientLayer}s.
+ * 
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
+public class SkyGradientLayerWrapper extends ClassLayerWrapper<SkyGradientLayer>
+{
+	private final static String ATMOSPHERE_THICKNESS_ELEMENT = "AtmosphereThickness"; //$NON-NLS-1$
+	private final static String HORIZON_COLOR_ELEMENT = "HorizonColor"; //$NON-NLS-1$
+	private final static String ZENITH_COLOR_ELEMENT = "ZenithColor"; //$NON-NLS-1$
+
+	@Override
+	protected Class<SkyGradientLayer> getLayerClass()
+	{
+		return SkyGradientLayer.class;
+	}
+
+	@Override
+	protected void load(SkyGradientLayer layer, Element element)
+	{
+		XPath xpath = WWXML.makeXPath();
+
+		Double atmosphereThickness = WWXML.getDouble(element, ATMOSPHERE_THICKNESS_ELEMENT, xpath);
+		if (atmosphereThickness != null)
+		{
+			layer.setAtmosphereThickness(atmosphereThickness);
+		}
+
+		Color horizonColor = WWXML.getColor(element, HORIZON_COLOR_ELEMENT, xpath);
+		if (horizonColor != null)
+		{
+			layer.setHorizonColor(horizonColor);
+		}
+
+		Color zenithColor = WWXML.getColor(element, ZENITH_COLOR_ELEMENT, xpath);
+		if (zenithColor != null)
+		{
+			layer.setZenithColor(zenithColor);
+		}
+	}
+
+	@Override
+	protected void save(SkyGradientLayer layer, Element element)
+	{
+		WWXML.appendDouble(element, ATMOSPHERE_THICKNESS_ELEMENT, layer.getAtmosphereThickness());
+		XMLUtil.appendColor(element, HORIZON_COLOR_ELEMENT, layer.getHorizonColor());
+		XMLUtil.appendColor(element, ZENITH_COLOR_ELEMENT, layer.getZenithColor());
+	}
+
+	public double getAtmosphereThickness()
+	{
+		return getLayer().getAtmosphereThickness();
+	}
+
+	public void setAtmosphereThickness(double atmosphereThickness)
+	{
+		getLayer().setAtmosphereThickness(atmosphereThickness);
+	}
+
+	public Color getHorizonColor()
+	{
+		return getLayer().getHorizonColor();
+	}
+
+	public void setHorizonColor(Color horizonColor)
+	{
+		getLayer().setHorizonColor(horizonColor);
+	}
+
+	public Color getZenithColor()
+	{
+		return getLayer().getZenithColor();
+	}
+
+	public void setZenithColor(Color zenithColor)
+	{
+		getLayer().setZenithColor(zenithColor);
+	}
+}
