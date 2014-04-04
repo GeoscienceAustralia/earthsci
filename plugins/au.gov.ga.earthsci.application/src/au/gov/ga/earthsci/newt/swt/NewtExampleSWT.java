ae673b7f38bcbc09fc9c61af72099676f10b3075
Michael de Hoog <michael.dehoog@ga.gov.au>
2014-04-04 14:40:51 +1100

    Switched the SWT NEWT example to use the SWT World Window instead of the SWT_AWT bridge

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/newt/swt/NewtExampleSWT.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/newt/swt/NewtExampleSWT.java
@@ -20,19 +20,10 @@ import gov.nasa.worldwind.Model;
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.avlist.AVKey;
 
-import java.awt.BorderLayout;
-import java.awt.Frame;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-
-import au.gov.ga.earthsci.newt.awt.NewtInputHandlerAWT;
-import au.gov.ga.earthsci.newt.awt.WorldWindowNewtAutoDrawableAWT;
-import au.gov.ga.earthsci.newt.awt.WorldWindowNewtCanvasAWT;
 
 /**
  * Example of using the {@link WorldWindowNewtCanvasSWT}.
@@ -47,18 +38,18 @@ public class NewtExampleSWT
 		Shell shell = new Shell(display);
 		shell.setLayout(new FillLayout());
 
-		/*System.setProperty("newt.debug.Window", "true");
+		//System.setProperty("newt.debug.Window", "true");
 		Configuration.setValue(AVKey.INPUT_HANDLER_CLASS_NAME, NewtInputHandlerSWT.class.getName());
 		Configuration.setValue(AVKey.WORLD_WINDOW_CLASS_NAME, WorldWindowNewtAutoDrawableSWT.class.getName());
-		WorldWindowNewtCanvasSWT wwd = new WorldWindowNewtCanvasSWT(shell, SWT.NONE, null);*/
+		WorldWindowNewtCanvasSWT wwd = new WorldWindowNewtCanvasSWT(shell, SWT.NONE, null);
 
-		Composite composite = new Composite(shell, SWT.EMBEDDED);
+		/*Composite composite = new Composite(shell, SWT.EMBEDDED);
 		Frame frame = SWT_AWT.new_Frame(composite);
 		frame.setLayout(new BorderLayout());
 		Configuration.setValue(AVKey.INPUT_HANDLER_CLASS_NAME, NewtInputHandlerAWT.class.getName());
 		Configuration.setValue(AVKey.WORLD_WINDOW_CLASS_NAME, WorldWindowNewtAutoDrawableAWT.class.getName());
 		WorldWindowNewtCanvasAWT wwd = new WorldWindowNewtCanvasAWT();
-		frame.add(wwd, BorderLayout.CENTER);
+		frame.add(wwd, BorderLayout.CENTER);*/
 
 		Model m = (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
 		wwd.setModel(m);
