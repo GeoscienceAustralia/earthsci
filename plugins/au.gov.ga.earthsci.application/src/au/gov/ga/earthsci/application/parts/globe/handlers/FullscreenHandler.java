d8a8ba2e71b3a2734b7026f77f5a167d8d60167b
Michael de Hoog <michael.dehoog@ga.gov.au>
2014-04-30 15:25:12 +1000

    Added correct fullscreen handling

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/parts/globe/handlers/FullscreenHandler.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/parts/globe/handlers/FullscreenHandler.java
@@ -17,7 +17,17 @@ package au.gov.ga.earthsci.application.parts.globe.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+import au.gov.ga.earthsci.newt.swt.WorldWindowNewtCanvasSWT;
 
 /**
  * Handler for the fullscreen command.
@@ -26,11 +36,75 @@ import org.eclipse.swt.widgets.Composite;
  */
 public class FullscreenHandler
 {
+	private Shell fullscreenShell;
+
 	@Execute
-	public void execute(Composite parent, MToolItem item)
+	public void execute(final Composite parent, final MToolItem item)
 	{
-		boolean fullscreen = !parent.getShell().getFullScreen();
-		parent.getShell().setFullScreen(fullscreen);
-		item.setSelected(fullscreen);
+		//already fullscreen, dispose of fullscreen shell
+		if (fullscreenShell != null)
+		{
+			fullscreenShell.dispose();
+			return;
+		}
+
+		WorldWindowNewtCanvasSWT wwd = null;
+		for (Control child : parent.getChildren())
+		{
+			if (child instanceof WorldWindowNewtCanvasSWT)
+			{
+				wwd = (WorldWindowNewtCanvasSWT) child;
+				break;
+			}
+		}
+
+		if (wwd != null)
+		{
+			fullscreenShell = new Shell(parent.getDisplay(), SWT.CLOSE | SWT.MAX);
+			fullscreenShell.setLayout(new FillLayout());
+			final WorldWindowNewtCanvasSWT wwdFinal = wwd;
+			wwd.setParent(fullscreenShell);
+
+			parent.addDisposeListener(new DisposeListener()
+			{
+				@Override
+				public void widgetDisposed(DisposeEvent e)
+				{
+					if (fullscreenShell != null && !fullscreenShell.isDisposed())
+					{
+						fullscreenShell.dispose();
+					}
+				}
+			});
+			fullscreenShell.addDisposeListener(new DisposeListener()
+			{
+				@Override
+				public void widgetDisposed(DisposeEvent e)
+				{
+					if (!parent.isDisposed())
+					{
+						wwdFinal.setParent(parent);
+						parent.layout();
+					}
+					fullscreenShell = null;
+					item.setSelected(false);
+				}
+			});
+			fullscreenShell.addTraverseListener(new TraverseListener()
+			{
+				@Override
+				public void keyTraversed(TraverseEvent e)
+				{
+					if (e.detail == SWT.TRAVERSE_ESCAPE)
+					{
+						fullscreenShell.dispose();
+					}
+				}
+			});
+
+			fullscreenShell.setVisible(true);
+			fullscreenShell.setFullScreen(true);
+			item.setSelected(true);
+		}
 	}
 }
