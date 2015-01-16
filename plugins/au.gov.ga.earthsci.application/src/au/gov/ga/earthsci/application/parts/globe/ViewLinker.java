5092feb574b8e1d73ab30b0cc9e5cda3626a6535
Michael de Hoog <michael.dehoog@ga.gov.au>
2015-01-16 13:34:36 +1100

    Fixed bug where view was being added instead of removed when unlinked

--- plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/parts/globe/ViewLinker.java
+++ plugins/au.gov.ga.earthsci.application/src/au/gov/ga/earthsci/application/parts/globe/ViewLinker.java
@@ -57,7 +57,7 @@ public class ViewLinker implements PropertyChangeListener
 	public synchronized void unlink(View view)
 	{
 		view.removePropertyChangeListener(AVKey.VIEW, this);
-		views.add(view);
+		views.remove(view);
 	}
 
 	public boolean isEnabled()
