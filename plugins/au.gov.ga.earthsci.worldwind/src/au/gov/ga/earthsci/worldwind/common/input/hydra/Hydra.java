92fc3a3d684c7a480efa0cdc4f0f45b4e8f20813
Michael de Hoog <michael.dehoog@ga.gov.au>
2015-01-19 15:10:39 +1100

    Switched sixense library loading to the SixenseActivator

--- plugins/au.gov.ga.earthsci.worldwind/src/au/gov/ga/earthsci/worldwind/common/input/hydra/Hydra.java
+++ plugins/au.gov.ga.earthsci.worldwind/src/au/gov/ga/earthsci/worldwind/common/input/hydra/Hydra.java
@@ -17,6 +17,7 @@ package au.gov.ga.earthsci.worldwind.common.input.hydra;
 
 import com.sixense.ControllerData;
 import com.sixense.Sixense;
+import com.sixense.SixenseActivator;
 import com.sixense.utils.ControllerManager;
 import com.sixense.utils.enums.EnumGameType;
 import com.sixense.utils.enums.EnumSetupStep;
@@ -94,44 +95,7 @@ public class Hydra
 
 	private static boolean loadLibrary()
 	{
-		try
-		{
-			Error error = null;
-			try
-			{
-				System.loadLibrary("sixense");
-				System.loadLibrary("sixense_utils");
-			}
-			catch (Error e)
-			{
-				error = e;
-			}
-			if (error != null)
-			{
-				//loading 32-bit didn't work, try 64-bit
-				try
-				{
-					System.loadLibrary("sixense_x64");
-					System.loadLibrary("sixense_utils_x64");
-					error = null;
-				}
-				catch (Error e)
-				{
-				}
-			}
-			if (error != null)
-			{
-				throw error;
-			}
-			System.loadLibrary("SixenseJava");
-		}
-		catch (Throwable e)
-		{
-			System.err.println("Razer Hydra disabled: unable to load SixenseJava library");
-			e.printStackTrace();
-			return false;
-		}
-		return true;
+		return SixenseActivator.isLibraryLoaded();
 	}
 
 	private void startPolling()
